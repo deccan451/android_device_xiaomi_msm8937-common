d67254e072f8c3cfec3501993589333620818fc8
deccan451 <khans.2k8@gmail.com>
2020-08-26 07:38:35 +0000

    few more caf changes

--- msm8937.mk
+++ msm8937.mk
@@ -317,7 +317,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.2-service \
+    power.qcom
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
